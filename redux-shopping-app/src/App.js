1c93e55face359abd671ed5c0f9784d367bd0906
xinqiang-hu <danxqhu@gmail.com>
2023-03-03 17:53:00 +0800

    add fetch

--- redux-shopping-app/src/App.js
+++ redux-shopping-app/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Auth from './components/Auth';
 import Layout from './components/Layout';
 import { useSelector } from 'react-redux';
 
 function App() {
+  const cart = useSelector(state => state.cart);
   const islogin = useSelector(state => state.auth.islogin);
   // console.log(islogin);
   const cartItems = useSelector(state => state.cart.itemsList);
+  useEffect(() => {
+    fetch('', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   // console.log(cartItems);
   return (
     <div className="App">
